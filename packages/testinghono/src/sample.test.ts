2b76472e06d9e49f6efb096cdcb98ceb316d0cc3
kazurayam <kazuaki.urayama@gmail.com>
2026-01-31 08:58:47 +0900

    #12

--- packages/testinghono/src/sample.test.ts
+++ packages/testinghono/src/sample.test.ts
@@ -53,5 +53,36 @@ describe('Example', () => {
         const json = await res.json()
         expect(json).toEqual({ message: 'Post created' })
     })
-
+    test('POST /posts using Request class with non-existent URL', async () => {
+        const req = new Request('https://127.1.2.3:8080/posts', {
+            // It seems that the app (instance of Hono) will ignore the protocol(https), hostname(127.1.2.3) and port(8080)
+            // Am I right?
+            method: 'POST'
+        })
+        const res = await app.request(req)
+        expect(res.status).toBe(201)
+        expect(res.headers.get('X-Custom-Header')).toBe('Thank you')
+        const json = await res.json()
+        expect(json).toEqual({ message: 'Post created' })
+    })
+    test('POST /posts using Request class with syntactically valid but ridiculous URL', async () => {
+        const req = new Request('protocol://hostname:9999/posts', {
+            // It seems that the app (instance of Hono) will ignore the protocol(https), hostname(127.1.2.3) and port(8080)
+            // Am I right?
+            method: 'POST'
+        })
+        const res = await app.request(req)
+        expect(res.status).toBe(201)
+        expect(res.headers.get('X-Custom-Header')).toBe('Thank you')
+        const json = await res.json()
+        expect(json).toEqual({ message: 'Post created' })
+    })
+    test('POST /posts using Request class with invalid path URL will return 404 Not Found', async () => {
+        const req = new Request('protocol://hostname:9999/postzzz', {
+            // The path '/postzzz' is invalid
+            method: 'POST'
+        })
+        const res = await app.request(req)
+        expect(res.status).toBe(404) // Not Found
+    })
 })
